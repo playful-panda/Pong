af27eab6967f3be644e8daf771fcd9eebc60bd4e
Lucy Skowron <lucy_skowron@hotmail.com>
2019-03-24 16:51:23 +0000

    Setup the project

---
+++ Assets/Scripts/MovingBamboo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovingBamboo : MonoBehaviour {
+    private Rigidbody rb;
+    public float speed;
+
+    // Start is called before the first frame update
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() {
+    
+        float direction = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(0.0f, 0.0f, direction);
+
+        rb.AddForce(movement * speed);
+
+    }
+}
